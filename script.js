64e94d786e98c1e573d0fae2eb1af0abe00898bd
Emily Orry Bendtzen <emilybendtzen@MacBook-Pro-tilhrende-Emily.local>
2021-01-06 21:54:20 +0100

    Andet commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const knap = document.querySelector('#knap');
+const box = document.querySelector('nav');
+
+knap.addEventListener('click', toggle);
+
+function toggle() {
+    box.classList.toggle('toggle');
+}
+
+
